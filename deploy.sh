86d7ef0b301fd846264784629e06b0e3232d834d
verzac <benjamin.tanone@gmail.com>
2021-06-02 13:43:36 +0700

    chore: update lambda runtime to nodejs14.x

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ PARAMETER_OVERRIDES_ARG=""
 STACK_NAME="about-api-serverless-prod"
 
 sam validate
-sam package --template-file $TEMPLATE_FILENAME --s3-bucket $S3_BUCKET_NAME --output-template $PACKAGED_TEMPLATE_FILENAME  $AWSCLI_ARGS
+sam package --template-file $TEMPLATE_FILENAME --s3-bucket $S3_BUCKET_NAME --output-template-file $PACKAGED_TEMPLATE_FILENAME  $AWSCLI_ARGS
 sam deploy --template-file $PACKAGED_TEMPLATE_FILENAME --stack-name $STACK_NAME --capabilities CAPABILITY_IAM --no-fail-on-empty-changeset $AWSCLI_ARGS $PARAMETER_OVERRIDES_ARG
